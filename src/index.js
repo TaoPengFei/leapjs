1acbee0510573a3c2cd8b6270fc3e25fb777dc68
xrd#ub <305880887@qq.com>
2017-10-22 22:17:26 +0800

    add Animations

--- src/index.js
+++ src/index.js
@@ -3,18 +3,13 @@ var  ctx = require('./canvas.js').ctx;
 
 var shapes = require('./shapes.js');
 
-function drawLine(x1, y1, x2, y2, c){
-  var line = new Line(x1, y1, x2, y2);
-  line.strokeType = c;
-  line.draw();
-  return line;
-}
-
 window.canvas = canvas;
 window.ctx = ctx;
 window.Line = shapes.Line;
 window.Rectangle = shapes.Rectangle;
 window.Polygon = shapes.Polygon;
+window.Triangle = shapes.Triangle;
+window.Circle = shapes.Circle;
+window.Text = shapes.Text;
 window.Sprite = shapes.Sprite;
-
-window.drawLine = drawLine;
+window.Animation = shapes.Animation;
--- src/shapes.js
+++ src/shapes.js
@@ -24,15 +24,32 @@ Shape.prototype.stroke = function(){
 Shape.prototype.fill = function(){
     ctx.save();
     ctx.fillStyle = this.fillStyle;
+    ctx.beginPath();
     this._draw();
     ctx.fill();
     ctx.restore();
 };
 
-Shape.prototype.draw = Shape.prototype.stroke;
+Shape.prototype.draw = function(){
+    this.fill();
+    this.stroke();
+}
+
+function Circle(x, y, r){
+    Shape.call(this);
+    this.x = x;
+    this.y = y;
+    this.r = r;
+}
+
+inheritPrototype(Circle, Shape);
+
+Circle.prototype._draw = function(){
+    ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
+}
 
 function Line(x1, y1, x2, y2){
-    Shape.call(this, 0, 0);
+    Shape.call(this);
     this.x1 = x1;
     this.y1 = y1;
     this.x2 = x2;
@@ -46,11 +63,9 @@ Line.prototype._draw = function(){
     ctx.lineTo(this.x2, this.y2);
 };
 
-Line.prototype.stroke = Line.prototype.draw;
-
 function Polygon(){
     if(arguments.length < 6){
-        throw "not enough arguments";
+        throw "Polygon should have at lease 3 points";
     }
 
     this.points = [];
@@ -65,7 +80,6 @@ function Polygon(){
 inheritPrototype(Polygon, Shape);
 
 Polygon.prototype._draw = function(){
-    ctx.beginPath();
     var p = this.points[0];
     ctx.moveTo(p.x, p.y);
     for(var i=1; i<this.points.length; i++){
@@ -74,6 +88,12 @@ Polygon.prototype._draw = function(){
     }
     ctx.closePath();
 };
+
+function Triangle(x1, y1, x2, y2, x3, y3){
+    Polygon.call(this, x1, y1, x2, y2, x3, y3);
+}
+
+inheritPrototype(Triangle, Polygon);
 
 function Rectangle(x, y, w, h){
     Shape.call(this);
@@ -89,25 +109,82 @@ Rectangle.prototype._draw = function(){
     ctx.rect(this.x, this.y, this.w, this.h);
 };
 
+function Text(text, x, y, font){
+    this.text = text;
+    this.x = x;
+    this.y = y;
+    this.font = font || "20px Arial";
+}
+
+Text.prototype.stroke = function(){
+    ctx.font = this.font;
+    ctx.strokeText(this.text, this.x, this.y);
+}
+
+Text.prototype.fill = function(){
+    ctx.font = this.font;
+    ctx.fillText(this.text, this.x, this.y);
+}
+
 function Sprite(src, x, y, w, h){
     this.img = new Image();
     this.img.src = src;
-    this.x = x;
-    this.y = y;
+    this.x = x || 0;
+    this.y = y || 0;
     this.w = w;
     this.h = h;
-    this.img.onload = function(){
-        console.log("resouce loaded.");
-    };
+
+}
+
+Sprite.prototype.cut = function(sx, sy, sw, sh){
+    this.sx = sx;
+    this.sy = sy;
+    this.sw = sw;
+    this.sh = sh;
+    this.w = this.w || this.sw;
+    this.h = this.h || this.sh;
 }
 
 Sprite.prototype.draw = function(){
-    ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+    if(this.sx && this.sy && this.sw & this.sh){
+        ctx.drawImage(this.img, this.sx, this.sy, this.sw, this.sh,
+            this.x, this.y, this.w, this.h);
+    }
+    else if(this.w && this.h)
+        ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+    else
+        ctx.drawImage(this.img, this.x, this.y);
 };
+
+function Animation(src, x, y, w, h){
+    Sprite.call(this, src, x, y, w, h);
+}
+
+inheritPrototype(Animation, Sprite);
+
+Animation.prototype.setFrame = function(sw, sh, nf, speed){
+    if(nf %1 != 0)
+        throw "LLEG: setFrame(w, h, f, s), f must be an integer";
+    this.nf = nf;// frame count
+    this.cf = 0; // current frame
+    this.speed = speed > 1 ? speed : 1;
+    this.cut(1, 1, sw, sh);
+}
+
+Animation.prototype.draw = function(){
+    var sx = this.sx + this.sw * Math.floor(this.cf/this.speed % this.nf);
+    ctx.drawImage(this.img, sx, this.sy, this.sw, this.sh,
+        this.x, this.y, this.w, this.h);
+    this.cf++;
+}
 
 module.exports = {
     Line: Line,
     Rectangle: Rectangle,
     Polygon: Polygon,
-    Sprite: Sprite
+    Triangle: Triangle,
+    Circle: Circle,
+    Text: Text,
+    Sprite: Sprite,
+    Animation: Animation
 };
