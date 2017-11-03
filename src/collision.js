af4a8a9f71c466b62ad658dc95e0eac5681bf803
xrd#ub <305880887@qq.com>
2017-11-03 21:47:47 +0800

    add max min in util

--- src/collision.js
+++ src/collision.js
@@ -1,3 +1,7 @@
+// detect collision using shadows
+// p: Point, {x, y}
+// ps: Points, Array
+// rect: {minX, maxX, minY, maxY}
 var shapes = require('./shapes.js');
 var ctx = require('./canvas.js').ctx;
 var Mouse = require('./mouse.js').Mouse;
@@ -16,14 +20,13 @@ function collide(shape1, shape2){
     r1 = getRectShape(ps1);
     r2 = getRectShape(ps2);
  
-    if(r1.minX > r2.maxX || r1.minY > r2.maxY || r2.minX > r1.maxX || r2.minY > r1.maxY){
+    if(r1.minX > r2.maxX || r1.minY > r2.maxY || 
+        r2.minX > r1.maxX || r2.minY > r1.maxY)
         return false;
-    }
     // quick check end
     
     // if point inside shapes, return point
     ctx.drawPathByPoints(ps2);
-
     for(i=0; i<ps1.length; i++){
         p = ps1[i];
         if(pointInRect(p, r2) && ctx.isPointInPath(p.x, p.y)) return p;
@@ -34,10 +37,9 @@ function collide(shape1, shape2){
         p = ps2[i];
         if(pointInRect(p, r1) && ctx.isPointInPath(p.x, p.y)) return p;
     }
-
     // point check end
 
-    // vector
+    // vector shadow
     var u;
     for(i=0; i<ps1.length; i++){
         p1 = ps1[i];
@@ -65,56 +67,35 @@ function collide(shape1, shape2){
 }
 
 function shadowCollide(ps1, ps2, u){
-    var ps1min, ps1max, ps2min, ps2max;
+    var s1 = ps1.map(function(p){ return p.x*u.x + p.y*u.y; });
+    var s2 = ps2.map(function(p){ return p.x*u.x + p.y*u.y; });
 
-    for(i=0; i<ps1.length; i++){
-        p = ps1[i];
-        l = p.x*u.x + p.y*u.y;
-        if(ps1min === null) ps1min = l;
-        if(ps1max === null) ps1max = l;
-        ps1min = ps1min < l ? ps1min : l;
-        ps1max = ps1max > l ? ps1max : l;
-    }
-    for(i=0; i<ps2.length; i++){
-        p = ps2[i];
-        l = p.x*u.x + p.y*u.y;
-        if(ps2min === null) ps2min = l;
-        if(ps2max === null) ps2max = l;
-        ps2min = ps2min < l ? ps2min : l;
-        ps2max = ps2max > l ? ps2max : l;
-    }
-
-    if(ps1min > ps2max || ps1max < ps2min) return false;
+    if(s1.min() > s2.max() || s1.max() < s2.min()) return false;
 
     return true;
 }
 
 function getVerticalVector(p1, p2){
-
-    var u = {};
-    u.x = p1.y - p2.y;
-    u.y = p2.x - p1.x;
-
-    return u;
+    return {
+        x: p1.y-p2.y, 
+        y: p2.x-p1.x
+    };
 }
 
 function getRectShape(ps){
-    var rect = {};
-    rect.minX = rect.maxX = ps[0].x;
-    rect.minY = rect.maxY = ps[0].y;
 
-    ps.map(function(p){
-        rect.minX = rect.minX < p.x ? rect.minX : p.x;
-        rect.maxX = rect.maxX > p.x ? rect.maxX : p.x;
-        rect.minY = rect.minY < p.y ? rect.minY : p.y;
-        rect.maxY = rect.maxY > p.y ? rect.maxY : p.y;
-    });
+    var xs = ps.map(function(p){ return p.x });
+    var ys = ps.map(function(p){ return p.y });
 
-    return rect;
+    return {
+        minX: xs.min(), maxX: xs.max(),
+        minY: ys.min(), maxY: ys.max()
+    };
 }
 
-function pointInRect(p, rect){
-    return p.x > rect.minX && p.x < rect.maxX && p.y > rect.minY && p.y < rect.maxY;
+function pointInRect(p, r){
+    return r.minX < p.x && p.x < r.maxX 
+        && r.minY < p.y && p.y < r.maxY;
 }
 
 function pointInShape(p, shape){
--- src/util.js
+++ src/util.js
@@ -11,6 +11,14 @@ Array.prototype.contain = function(obj){
             return true;
     }
     return false;
+};
+
+Array.prototype.max = function(){
+    return Math.max.apply(null, this);
+};
+
+Array.prototype.min = function(){
+    return Math.min.apply(null, this);
 }
 
 module.exports = {
