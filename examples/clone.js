971bec84d2345971dcc735910143698983fcef65
xrd#ub <305880887@qq.com>
2017-11-12 01:39:35 +0800

    add clone examples and gravity & collison example

---
+++ examples/clone.js
@@ -0,0 +1,16 @@
+var url = "http://llcs-1252287760.cossh.myqcloud.com/";
+var bird = new Animation(url+"animations/bird.png", 80, 120, 56, 42);
+bird.setFrame(0, 0, 112, 84, 8, 1); // shiftX, shiftY, frameX, frameY, columns, rows
+
+var bird2 = bird.clone();
+bird2.y += 100;
+bird2.setSpeed(20);
+
+function main(){
+    canvas.clear();
+    bird.draw();
+    bird2.draw();
+    nextFrame(main);
+}
+
+loadRssAndRun(main);
--- examples/gravityAndCollision.js
+++ examples/gravityAndCollision.js
@@ -1,8 +1,9 @@
 var r = 20;
 var cs = [];
-var count = 60;
+var count = 80;
 
-function update(){
+function update(j){
+    this.y += 1;
     for(var i=0; i<count; i++){
         c = cs[i];
         if(c == this) continue;
@@ -15,19 +16,20 @@ function update(){
            	var dx = (2*r - d)/(d + 1e-6) * (this.x-c.x);
             var dy = (2*r - d)/(d + 1e-6) * (this.y-c.y);
 
-            this.x += dx/3 + Math.random() - 0.5;
-            this.y += dy/3 + Math.random() - 0.5;
+            this.x += dx/3;
+            this.y += dy/3;
+            c.x -= dx/3;
+            c.y -= dy/3;
         }
-
-        if(this.x < r) this.x = r;
-        if(this.y < r) this.y = r;
-        if(this.x + r > canvas.width) this.x = canvas.width - r;
-        if(this.y + r > canvas.height) this.y = canvas.height - r;
     }
+    if(this.x < r) this.x = r;
+    if(this.y < r) this.y = r;
+    if(this.x + r > canvas.width) this.x = canvas.width - r;
+    if(this.y + r > canvas.height) this.y = canvas.height - r;   
 }
 
 for(var i=0; i<count; i++){
-    c = new Circle(170, 270, r);
+    c = new Circle(150+50*Math.random(), 150+50*Math.random(), r);
     c.update = update;
     cs.push(c);
 }
@@ -36,7 +38,7 @@ for(var i=0; i<count; i++){
     canvas.clear();
 
     for(var i=0; i<cs.length; i++){
-        cs[i].update();
+        cs[i].update(i);
         cs[i].draw();
     }
 
