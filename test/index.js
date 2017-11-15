3ea2d17452905eea01ea776d9fb4d03513079404
xrd#ub <305880887@qq.com>
2017-11-15 23:12:43 +0800

    add test module: mocha

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Mocha', function(){
+    describe('Ready', function(){
+        it('mocha should be ready to go', function(){
+            assert.ok(true);
+        })
+    })
+})
