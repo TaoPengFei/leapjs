218aff93afdfbe49481a1686fdcc42fc4121834a
Vic Wang <305880887@qq.com>
2017-12-14 19:35:52 +0800

    use es6 style

--- src/changingNumber.js
+++ src/changingNumber.js
@@ -1,46 +1,45 @@
 class Swing {
   constructor (min, max, speed) {
-    this.min = min;
-    this.max = max;
-    this.cycle = (max - min)/speed * 2000;
-    this.t = new Date().getTime();
+    this.min = min
+    this.max = max
+    this.cycle = (max - min) / speed * 2000
+    this.t = new Date().getTime()
   }
 
-  [Symbol.toPrimitive](hint) {
-    var deltaT = (new Date().getTime() - this.t) % this.cycle;
-    var x = (this.max - this.min) * 2 * deltaT / this.cycle + this.min;
-    if(deltaT > this.cycle/2) return 2*this.max - x;
-    else return x;
+  [Symbol.toPrimitive] (hint) {
+    var deltaT = (new Date().getTime() - this.t) % this.cycle
+    var x = (this.max - this.min) * 2 * deltaT / this.cycle + this.min
+    if (deltaT > this.cycle / 2) return 2 * this.max - x
+    else return x
   }
-
 }
 
 class Increase {
   constructor (min, max, speed) {
-    this.min = min;
-    this.max = max;
-    this.cycle = (max - min)/speed * 1000;
-    this.t = new Date().getTime();
+    this.min = min
+    this.max = max
+    this.cycle = (max - min) / speed * 1000
+    this.t = new Date().getTime()
   }
 
-  [Symbol.toPrimitive](hint) {
-    var deltaT = (new Date().getTime() - this.t) % this.cycle;
-    return (this.max - this.min) * deltaT / this.cycle + this.min;
+  [Symbol.toPrimitive] (hint) {
+    var deltaT = (new Date().getTime() - this.t) % this.cycle
+    return (this.max - this.min) * deltaT / this.cycle + this.min
   }
 }
 
 class Sine {
   constructor (min, max, speed) {
-    this.min = min;
-    this.max = max;
-    this.cycle = (max - min)/speed * 2000;
-    this.t = new Date().getTime();
+    this.min = min
+    this.max = max
+    this.cycle = (max - min) / speed * 2000
+    this.t = new Date().getTime()
   }
 
-  [Symbol.toPrimitive](hint) {
-    var deltaT = (new Date().getTime() - this.t) % this.cycle;
-    return (this.max + this.min) / 2 + 
-      (this.max - this.min) / 2 * Math.sin(deltaT/this.cycle * Math.PI * 2)
+  [Symbol.toPrimitive] (hint) {
+    var deltaT = (new Date().getTime() - this.t) % this.cycle
+    return (this.max + this.min) / 2 +
+      (this.max - this.min) / 2 * Math.sin(deltaT / this.cycle * Math.PI * 2)
   }
 }
 
