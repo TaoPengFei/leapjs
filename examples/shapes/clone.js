5c8010d928f45d7033d3aa264472faaa88f1d9c1
Vic Wang <305880887@qq.com>
2018-03-17 17:03:58 +0800

    add example list

--- examples/shapes/clone.js
+++ examples/shapes/clone.js
@@ -11,7 +11,7 @@ var url = "https://llcs-1252287760.cossh.myqcloud.com/";
 var bird = new Animation(url+"animations/bird.png", 80, 120, 56, 42);
 bird.setFrame(0, 0, 112, 84, 8, 1); // shiftX, shiftY, frameX, frameY, columns, rows
 
-var bird2 = bird.clone();  // 返回一个克隆体
+var bird2 = bird.clone();
 
 bird2.y += 100;
 bird2.setSpeed(20);
--- examples/styles/const.js
+++ examples/styles/const.js
@@ -1,13 +1,13 @@
-var colors = ["aqua", "black", "blue", "fuchsia", "gray", "green", "lime", "maroon", "navy", "olive", "purple", "red", "silver", "teal", "white", "yellow"];
+/***************************************************************
 
-var crect = new Rectangle(10, 10, 200, 25);
-var cname = new Text("color", 20, 10);
+颜色常量名
 
-for(var i=0; i<colors.length; i++){
-  crect.fillStyle = colors[i];
-  cname.src = colors[i];
-  crect.fill();
-  cname.fill();
-  crect.y += 30;
-  cname.y += 30;
-}
+使用字符常量来代表颜色，如 "red", "orange", "yellow", "green"等等
+
+
+更多参考资料：
+
+http://www.w3school.com.cn/tags/html_ref_colornames.asp
+
+***************************************************************/
+background("red");
